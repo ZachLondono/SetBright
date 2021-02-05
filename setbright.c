3ff9b1a4e469c427007f3585e8869f4ac1393ace
ZachLondono <zachary.londono5@gmail.com>
2021-02-04 23:56:08 -0500

    Added options to inc and dec

--- setbright.c
+++ setbright.c
@@ -1,3 +1,9 @@
+/*
+*	SetBright - changes the brightness of the laptops backlight
+*	* requires root privleges to set new brightness * 
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,127 +15,192 @@
 #include <unistd.h>
 #include <math.h>
 
-enum mode {Value, Set};
+// File where the brightness value needs to be saved
+const char* brightness_path = "/sys/class/backlight/intel_backlight/brightness";
+// File that contains the maximum brightness
+const char* brightness_max_path = "/sys/class/backlight/intel_backlight/max_brightness";
+
+enum mode {Value, Set, Inc, Dec};
+
+int set_by_val(long new_bright);
+long parse_argument_value(char* arg);
+long read_brightness(const char* path);
 
 int main (int argc, char** argv) {
 
+	enum mode exec_mode = Set;
+
 	if (argc == 1) {
 		fprintf(stderr, "Usage: %s brightness\n", argv[0]);
+		fprintf(stderr, "list options: %s --help \n", argv[0]);
 		exit(EXIT_FAILURE);
-	}
+	} else {
 
-	if (argc > 2) {
-		perror("Invalid usage, enter a brightness value 0-100\n");
-		fprintf(stderr, "Usage: %s brightness\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	// Check that the second argument is a number
-	char* bright_str = argv[1];
-	
-	enum mode execution_mode = Set;
-
-	if (strlen(bright_str) == 2) 
-		if (bright_str[0] == '-' && bright_str[1] == 'v') execution_mode = Value;
-
-	long new_bright = -1;
-	if (execution_mode == Set) {
-		// If mode is to set the brightness, the user must enter in a valid brightness value
-		errno = 0;
-		char* endptr;
-		new_bright = strtol(bright_str, &endptr, 10);
-		if ( (errno != 0 && new_bright == 0)			// Error parsing value 
-				|| endptr == bright_str 		// No numbers found
-				|| (new_bright < 0 || new_bright > 100) // Out of range
-				|| endptr[0] != '\0') {			// Extra non-digit characters TODO: check if endptr = '.' for fractional percent
-			perror("Brightness must be in range 0-100\n");
-			exit(EXIT_FAILURE);
+		char* flag = argv[1];
+		if (strcmp(flag, "--help") == 0|| strcmp(flag, "-h") == 0) {
+			
+			// ignore all other flags, just print help dialogue
+			printf("Usage: %s <brightness>\n\n"
+			"Adjust the brightness of the backlight\n\n"
+			"   -h, --help	Print options\n"
+			"   -v		Print current backlight brightness\n"
+			"   -inc VAL	increment brightness\n"
+			"   -dec VAL:	decrement brightness\n", argv[0]);
+				
+			exit(EXIT_SUCCESS);
+				
+		} if (strcmp(flag, "-v") == 0) {
+			exec_mode = Value;
+		} else if (strcmp(flag, "-inc") == 0) {
+			exec_mode = Inc;
+			if (argc != 3) {
+				printf("Invalid input\nUsage: %s -inc VAL\n", argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		} else if (strcmp(flag, "-dec") == 0) {
+			exec_mode = Dec;
+			if (argc != 3) {
+				printf("Invalid input\nUsage: %s -dec VAL\n", argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			exec_mode = Set;
+			if (argc != 2) {
+				printf("Invalid input\nUsage: %s VAL\n", argv[0]);
+				exit(EXIT_FAILURE);
+			}
 		}
+
 	}
 
 
-	// Opening file descriptors for the brightness file and max brightness file
-	char* brightness_path = "/sys/class/backlight/intel_backlight/brightness";
-	//char* brightness_path = "/home/zachary/Documents/Code/GTK/brightness/brightness_test";
-	char* brightness_max_path = "/sys/class/backlight/intel_backlight/max_brightness";
+	long max_bright = read_brightness(brightness_max_path);
+	if (max_bright == -1) perror("max");
 
-	int max_fd = open(brightness_max_path, O_RDONLY);
-	
-	if (max_fd == -1) {
-		perror("Could not change brightness value\n");
-		if (errno == EACCES) perror("Failed to access brightness value, check permissions\n");
-		else if (errno == ENOENT) perror("Brightness file does not exist\n");
-	
-		exit(EXIT_FAILURE);
-	}
-
-	// read the maximum brightness value
-	char max_bright_buff[10];
-	int count = 0;
-	int total = 0;
-	while ((count = read(max_fd, &max_bright_buff[total], 9)) != 0 && (total += count < 9))	{}
-	close(max_fd);
-	max_bright_buff[9] = '\0';
-
-	errno = 0;
-	char* max_endptr;
-	long max_bright = strtol(max_bright_buff, &max_endptr, 10);
-	if (errno != 0 && max_bright == 0) {
-		perror("Error reading maximum brightness value\n");
-		exit(EXIT_FAILURE);
-	}
-
-	if (execution_mode == Value) {
+	if (exec_mode == Value) {
 		
-		int bright_fd = open(brightness_path, O_RDONLY);
-		if (bright_fd == -1) {
-			perror("Could not change brightness value\n");
-			if (errno == EACCES) perror("Failed to access brightness value, check permissions\n");
-			else if (errno == ENOENT) perror("Brightness file does not exist\n");
-		
-			exit(EXIT_FAILURE);
-		}
-		
-		// Read in value from brightness
-		char curr_bright_buff[10];
-		size_t count = 0;
-		size_t total = 0;
-		while ((count = read(bright_fd, &curr_bright_buff[total], 9)) != 0 && (total += count < 9)) {}
-		curr_bright_buff[9] = '\0';
-		close(bright_fd);
-		
-		// Convert string to a number
-		errno = 0;
-		char* curr_endptr;
-		long curr_bright = strtol(curr_bright_buff, &curr_endptr, 10);
-		if (errno != 0 && curr_bright == 0) {
-			perror("Error reading current brightness value\n");
+		long curr_bright = read_brightness(brightness_path);
+		if (curr_bright < 0) {
+			perror("Failed to read current brightness value");
 			exit(EXIT_FAILURE);
 		}
 		
 		// Just print out brightness level as %
 		double brightness_percent = (double) curr_bright / (double) max_bright;
 		printf("Brightness: %f%%\n", brightness_percent * 100);
-
-	} else if (execution_mode == Set) {
 		
-		// Set new brightness value
+		exit(EXIT_SUCCESS);
 
-		long new_actual_value = max_bright * ((double) new_bright / 100);
+	} 
 
-		FILE* fptr = fopen(brightness_path, "w");
-		if (fptr == NULL) {
-			perror("Error setting brightness\n");
+
+	long new_bright = 1; // defualt to lowest without being completely off
+
+	if (exec_mode == Set) {
+		
+		// If mode is to set the brightness, the user must enter in a valid brightness value
+		long input = parse_argument_value(argv[1]);		
+		if (input < 0 || input > 100) {
+			perror("Invalid value, enter brightness in range 0-100");
 			exit(EXIT_FAILURE);
 		}
 
-		fprintf(fptr, "%ld", new_actual_value);
-		fclose(fptr);
+		new_bright = ((double) input / 100) * max_bright;
+		printf("new_bright: %ld\n", new_bright);
+		
+	} else {
 
-		printf("Brightness set to: %ld\n", new_actual_value);
+		long curr_bright = read_brightness(brightness_path);
+		if (curr_bright < 0) {
+			perror("Failed to read current brightness value");
+			exit(EXIT_FAILURE);
+		}
+
+		long input_raw = parse_argument_value(argv[2]);		
+		if (input_raw < 0 || input_raw > 100) {
+			perror("Invalid value, enter brightness in range 0-100");
+			exit(EXIT_FAILURE);
+		}
+
+		long input = ((double) input_raw / 100) * max_bright;
+
+		if (exec_mode == Inc) {
+			if (curr_bright + input > max_bright) 
+				new_bright = max_bright;
+			else new_bright = curr_bright + input;
+		} else if (exec_mode == Dec) {
+			if (curr_bright - input < 1) 
+				new_bright = 1;
+			else new_bright = curr_bright - input;
+		}
 
 	}
 	
-	exit(EXIT_SUCCESS);
+	int ret = set_by_val(new_bright);
+	if (ret < 0) {
+		perror("Failed to set brightness\n");
+		exit(EXIT_FAILURE);
+	}
 	
+}
+
+
+/*
+*	Set the brightness as a percentage of max brightness
+*/
+int set_by_val(long new_bright) {
+
+	FILE* fptr = fopen(brightness_path, "w");
+	if (fptr == NULL) {
+		return -1;
+	}
+
+	int ret = fprintf(fptr, "%ld", new_bright);
+	fclose(fptr);
+	
+	if (ret < 0) return -1;
+
+	return 0;
+
+}
+
+long parse_argument_value(char* arg) {
+
+	errno = 0;
+	char* endptr;
+	long parsed = strtol(arg, &endptr, 10);
+	if ( (errno != 0 && parsed == 0)				// Error parsing value 
+			|| endptr == arg 				// No numbers found
+			|| (endptr[0] != '\0' && endptr[0] != '\n')) {	// Extra non-digit characters TODO: check if endptr = '.' for fractional percent
+		return -1;
+	}
+
+	return parsed;
+
+}
+
+
+long read_brightness(const char* path) {
+
+	int fd = open(path, O_RDONLY);
+	if (fd == -1) {
+		perror("Could not change brightness value\n");
+		if (errno == EACCES) perror("Failed to access brightness value, check permissions\n");
+		else if (errno == ENOENT) perror("Brightness file does not exist\n");
+		return -1;
+	}
+
+	char* bright_buff = malloc(10);
+	int count = 0;
+	int total = 0;
+	while ((count = read(fd, &bright_buff[total], 9)) != 0 && (total += count < 9))	{}
+	close(fd);
+	bright_buff[9] = '\0';
+
+	long parsed = parse_argument_value(bright_buff);		
+	
+	free(bright_buff);
+
+	return parsed;
+
 }
